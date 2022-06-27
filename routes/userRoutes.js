22d9cb0def3fe21b890065f552472a2cb5b6929a
Maxi-Rios <maxirecibos182@gmail.com>
2022-06-27 18:48:34 -0300

    Cambios a modelo de user y nuevas rutas

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,5 +6,8 @@ router.post('/register', userController.registerUser);
 router.post('/login', userController.loginUser);
 router.get('/logout', userController.logoutUser);
 router.get('/getMe', requireAuth, userController.getMe);
+router.get('/admin/user', requireAuth, userController.getUsers);
+router.put('/update/:id', userController.userUpdate);
+router.delete('/remove/:id', userController.removeUser);
 
 module.exports = router;
