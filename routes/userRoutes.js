968fa2c70fa8d01c2a1583cfbadf87bf12369938
Maxi-Rios <maxirecibos182@gmail.com>
2022-06-28 17:04:42 -0300

    vistas de forgorPassword mym

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 const userController = require('../controllers/userController');
 const requireAuth = require('../middlewares/requireAuth');
 
-router.post('/register', userController.registerUser);
-router.post('/login', userController.loginUser);
 router.get('/logout', userController.logoutUser);
 router.get('/getMe', requireAuth, userController.getMe);
+router.get('/admin/user', requireAuth, userController.getUsers);
+router.post('/register', userController.registerUser);
+router.post('/login', userController.loginUser);
 router.post('/forgot-password', userController.forgotPassword);
 router.post('/:id/new-password', userController.createNewPassword);
-router.get('/admin/user', requireAuth, userController.getUsers);
 router.put('/update/:id', userController.userUpdate);
 router.delete('/remove/:id', userController.removeUser);
 
