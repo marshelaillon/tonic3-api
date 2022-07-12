911d915fd734d28848a936102ddff64de09d227d
marshel <marshel.aillon@gmail.com>
2022-07-12 12:38:30 -0400

    config db

--- db/index.js
+++ db/index.js
@@ -1,20 +1,25 @@
 const Sequelize = require('sequelize');
+let db;
 
-/* const db = new Sequelize('virtual_events', null, null, {
-  dialect: 'postgres',
-  host: 'localhost',
-  logging: false,
-}); */
-
-const db = new Sequelize(
-  'd6nimcqboqdga5',
-  'bwlmjybmuqnlve',
-  'd1d24e4faacb0d3359bab3ad31fb3ca06c60f1e2f89bc075eca523f7d6d0abd4',
-  {
+if (process.env.NODE_ENV === 'development') {
+  db = new Sequelize('virtual_events', null, null, {
     dialect: 'postgres',
-    host: 'ec2-54-160-109-68.compute-1.amazonaws.com',
+    host: 'localhost',
     logging: false,
-  }
-);
+  });
+}
+
+if (process.env.NODE_ENV === 'production') {
+  db = new Sequelize(
+    process.env.DATABASE_URL,
+    'bwlmjybmuqnlve',
+    'd1d24e4faacb0d3359bab3ad31fb3ca06c60f1e2f89bc075eca523f7d6d0abd4',
+    {
+      dialect: 'postgres',
+      host: 'ec2-54-160-109-68.compute-1.amazonaws.com',
+      logging: false,
+    }
+  );
+}
 
 module.exports = db;
