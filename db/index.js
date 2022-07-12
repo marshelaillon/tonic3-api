4a3aec8f2f16f01a91022c8685839872e2588cca
marshel <marshel.aillon@gmail.com>
2022-07-12 12:44:55 -0400

    curly braces closed

--- db/index.js
+++ db/index.js
@@ -19,10 +19,11 @@ if (process.env.NODE_ENV === 'production') {
       host: 'ec2-54-160-109-68.compute-1.amazonaws.com',
       logging: false,
       dialectOptions: {
-        ssl: { 
+        ssl: {
           require: true,
-          rejectUnauthorized: false 
-        }
+          rejectUnauthorized: false,
+        },
+      },
     }
   );
 }
