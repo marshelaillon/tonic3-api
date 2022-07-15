6e16e32d0bdb725fa65ba97e69a9542912e90e46
marshel <marshel.aillon@gmail.com>
2022-07-15 01:08:40 -0400

    demo

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,19 +2,16 @@ const router = require('express').Router();
 const { UserController, uploadFile } = require('../controllers/userController');
 const requireAuth = require('../middlewares/requireAuth');
 
-
 router.get('/logout', requireAuth, UserController.logoutUser);
 router.get('/getMe', requireAuth, UserController.getMe);
-// router.get('/admin/user', requireAuth, UserController.getAllUsers);
 router.post('/register', UserController.registerUser);
 router.post('/login', UserController.loginUser);
 router.post('/forgot-password', UserController.forgotPassword);
 router.post('/:id/new-password', UserController.createNewPassword);
 router.post('/update/:id', requireAuth, uploadFile, UserController.userUpdate);
 router.delete('/remove/:id', UserController.removeUser);
-//router.post("/register-with-hcaptcha", UserController.hcaptcha)
 
-
+// Faltan poner a Yaml
 router.post('/verify-email', UserController.verifyEmail);
 router.post('/verify-guest-token', UserController.verifyToken);
 router.put('/update-token', UserController.updateToken);
